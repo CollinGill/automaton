ac925d05cebdea24976e3ac61b00e8cc9f52f1e3
CollinGill <collingill02@gmail.com>
2022-11-23 22:36:07 -0500

    success register page w/redirection

--- automaton-client/src/pages/SignupSuccess.tsx
+++ automaton-client/src/pages/SignupSuccess.tsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 const SignupSuccess = () => {
-  return <div></div>;
+  return (
+    <div className="flex m-auto">
+      <p className="text-gray-900 text-xs m-auto py-5 font-semibold decoration-indigo-500">
+        Thank you for registering for Automaton! Click{" "}
+        <Link
+          to="/"
+          className="decoration-indigo-500 hover:underline underline-offset-4 font-bold"
+        >
+          here
+        </Link>{" "}
+        to return to the homepage and then proceed to login!
+      </p>
+    </div>
+  );
 };
 
 export default SignupSuccess;
