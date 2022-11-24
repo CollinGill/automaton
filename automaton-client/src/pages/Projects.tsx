813104665795951dd1c7324e510637e7b8ca5d91
CollinGill <collingill02@gmail.com>
2022-11-24 12:08:26 -0500

    basic outline div for projects home

--- automaton-client/src/pages/Projects.tsx
+++ automaton-client/src/pages/Projects.tsx
@@ -1,5 +1,10 @@
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col">
+      <div className="py-3" />
+      <div className="bg-gray-800 m-auto md:w-11/12 md:h-80 rounded-2xl border-gray-900 border-solid border-4"></div>
+    </div>
+  );
 };
 
 export default Projects;
