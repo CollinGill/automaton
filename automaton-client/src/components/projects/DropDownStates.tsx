259fa30dfc63348f299beedf92ad93cf0227a16b
CollinGill <collingill02@gmail.com>
2022-11-26 11:04:23 -0500

    dropdown statename selection

--- automaton-client/src/components/projects/DropDownStates.tsx
+++ automaton-client/src/components/projects/DropDownStates.tsx
@@ -6,28 +6,16 @@ interface DropDownStatesProps {
 // TODO: make a drop down button
 const DropDownStates = ({ text, stateNames }: DropDownStatesProps) => {
   return (
-    <div className="relative inline-block text-left">
-      <div className="py-4">
-        <button
-          className="inline-flex w-full justify-center rounded-md border border-indigo-500 m-auto px-2 py-1 text=gray-400"
-          aria-expanded={"true"}
-          aria-haspopup={"true"}
-        >
-          {text}
-          <svg
-            className="-mr-1 ml-2 h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </button>
+    <div className="relative inline-block text-left py-4">
+      <div className="flex py-1 px-2 justify-center items-center w-full border border-indigo-500 rounded-md shadow-sm outline-none appearance-none">
+        <div className="relative inline-block text-left">
+          <select className="w-full bg-transparent">
+            <option>{text}</option>
+            {stateNames.map((element) => (
+              <option>{element}</option>
+            ))}
+          </select>
+        </div>
       </div>
     </div>
   );
